12c4a48e6ecde2e92ced99e7da95cf112862feff
Saransh Bangar <114401238+codeinvador@users.noreply.github.com>
2023-08-16 22:47:56 +0530

    Add files via upload

---
+++ Task 3 (Completed)/script.js
@@ -0,0 +1,51 @@
+var quotes = 
+        [
+            "Be yourself; everyone else is already taken. - Oscar Wilde",
+            "The only way to do great work is to love what you do. - Steve Jobs",
+            "Life is what happens when you're busy making other plans. - John Lennon",
+            "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+            "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+            "In three words I can sum up everything I've learned about life: it goes on. - Robert Frost",
+            "Be yourself; everyone else is already taken. - Oscar Wilde",
+            "In three words I can sum up everything I've learned about life: it goes on. - Robert Frost",
+            "The only way to do great work is to love what you do. - Steve Jobs",
+            "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+            "Life is either a daring adventure or nothing at all. - Helen Keller",
+            "The only thing we have to fear is fear itself. - Franklin D. Roosevelt",
+            "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+            "The best way to predict the future is to create it. - Abraham Lincoln",
+            "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe. - Albert Einstein",
+            "Life is really simple, but we insist on making it complicated. - Confucius",
+            "To live is the rarest thing in the world. Most people exist, that is all. - Oscar Wilde",
+            "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",            "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+            "Believe you can and you're halfway there. - Theodore Roosevelt",
+            "You miss 100% of the shots you don't take. - Wayne Gretzky",
+            "The journey of a thousand miles begins with one step. - Lao Tzu",
+            "It does not matter how slowly you go as long as you do not stop. - Confucius",
+            "The only person you are destined to become is the person you decide to be. - Ralph Waldo Emerson",
+            "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
+            "I have not failed. I've just found 10,000 ways that won't work. - Thomas A. Edison",
+            "The future depends on what you do today. - Mahatma Gandhi",
+            "It always seems impossible until it's done. - Nelson Mandela",
+            "Success is walking from failure to failure with no loss of enthusiasm. - Winston Churchill",
+            "The harder the conflict, the greater the triumph. - George Washington",
+            "The way to get started is to quit talking and begin doing. - Walt Disney",
+            "Our greatest glory is not in never falling, but in rising every time we fall. - Confucius",
+            "You are never too old to set another goal or to dream a new dream. - C.S. Lewis",
+            "The secret of getting ahead is getting started. - Mark Twain",
+            "I can't change the direction of the wind, but I can adjust my sails to always reach my destination. - Jimmy Dean",
+            "A journey of a thousand miles begins with a single step. - Lao Tzu",
+            "It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
+            "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart. - Roy T. Bennett",
+            "You must be the change you wish to see in the world. - Mahatma Gandhi",
+            "Do not wait for leaders; do it alone, person to person. - Mother Teresa",
+            "The only true wisdom is in knowing you know nothing. - Socrates",
+            "Success is not in what you have, but who you are. - Bo Bennett",
+            "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+        ];
+
+        function generateQuote() {
+            var randomNumber = Math.floor(Math.random() * quotes.length);
+            var quoteElement = document.getElementById('quote');
+            quoteElement.textContent = quotes[randomNumber];
+        }
